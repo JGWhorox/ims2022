016f544962cba835bc4ca459551aca81ddaff645
Johann Gawron <horoxj@gmail.com>
2022-11-30 19:20:27 +0100

    added main declaration

---
+++ genevachecklist.h
@@ -0,0 +1,10 @@
+/**
+ * authors Johann A. Gawron - xgawro00, Patrik Slovák - xslova21
+ * project Military Simulator
+ * file genevachecklist.cpp
+ * brief implementation of header file for Military simulator main file
+ */
+
+#include "clifunctionality.h"
+
+int main(int argc, char *argv[]);
