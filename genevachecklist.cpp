a7002ea183f19bd8f0d791f616686c47c318f83f
Johann Gawron <horoxj@gmail.com>
2022-11-30 17:56:19 +0100

    added main func kekw

---
+++ genevachecklist.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+ 
+int main(int argc, char *argv[]){
+    std::cout << "Welcome to your week's purgatory" << std::endl;
+    return 0;
+}
