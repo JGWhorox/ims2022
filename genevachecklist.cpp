191063c213a5fe04a5877f68d0b0e3e17cf0ec7b
niccer56 <patrikslovak56@gmail.com>
2022-12-02 22:30:07 +0100

    added map generation in main

--- genevachecklist.cpp
+++ genevachecklist.cpp
@@ -12,6 +12,7 @@
 #include <string>
 
 #include "genevachecklist.h"
+#include "mapgenerator.h"
 
 int main(int argc, char** argv){
     //std::cout << "Welcome to your week's purgatory" << std::endl;
@@ -29,6 +30,18 @@ int main(int argc, char** argv){
     if(!parseArgs(args, input)){
         //std::cerr << "could not parse arguments, see help for more info" << std::endl;
         return -1;
+    }
+
+    MyMap map(25,25);
+    std::list<std::pair<int, int>> blue_line;
+    for (size_t y = 0; y < 25; y++)
+    {
+        //change for different generation
+        if (y < 15){
+            blue_line.push_back(std::make_pair(3,y));
+        } else {
+            blue_line.push_back(std::make_pair(4,y));
+        }
     }
 
     //parseConfigs
