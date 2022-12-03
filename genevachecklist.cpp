47bdf0c2ff885d2a2a4f9d80a99050abeb681a34
Johann Gawron <horoxj@gmail.com>
2022-12-03 18:32:22 +0100

    added army config part into the main program, should be easy to customize

--- genevachecklist.cpp
+++ genevachecklist.cpp
@@ -13,6 +13,7 @@
 
 #include "genevachecklist.h"
 #include "mapgenerator.h"
+#include "classes.h"
 
 int main(int argc, char** argv){
     //std::cout << "Welcome to your week's purgatory" << std::endl;
@@ -31,7 +32,7 @@ int main(int argc, char** argv){
         //std::cerr << "could not parse arguments, see help for more info" << std::endl;
         return -1;
     }
-
+    //setupScenario
     MyMap map(25,25);
     std::list<std::pair<int, int>> blue_line;
     for (size_t y = 0; y < 25; y++)
@@ -43,16 +44,46 @@ int main(int argc, char** argv){
             blue_line.push_back(std::make_pair(4,y));
         }
     }
+    map.set_blue_line(blue_line);
 
     //parseConfigs
 
-    //loadArmies
+    //configure armies 
+    Army blueArmy;
+    Army redArmy;
     
-    //setupScenario
+    //scuffed way of identifying armies in simulation algorithm #temporary #make your life easier, not harder
+    blueArmy.armyID = 1;
+    redArmy.armyID = 2;
+
+    //config of resources
+    blueArmy.ammo_supplies = 5000;
+    blueArmy.antitank_supplies = 500;
+    blueArmy.combat_supplies = 5000;
+    blueArmy.food_supplies = 10000;
+
+    redArmy.ammo_supplies = 5000;
+    redArmy.antitank_supplies = 500;
+    redArmy.combat_supplies = 5000;
+    redArmy.food_supplies = 10000;
+
+    blueArmy.logistics_effectivity = 95;
+    redArmy.logistics_effectivity = 80;
+
+    //config of army sizes
+    blueArmy.battalions.push_back(generate_batalion(4,150,1,50,1,10,blueArmy.armyID)); //600 Infantry + 50 Combat Support units + 10 Tanks with crew
+    blueArmy.battalions.push_back(generate_batalion(5,130,1,50,0,0,blueArmy.armyID));
+    blueArmy.battalions.push_back(generate_batalion(5,130,1,50,0,0,blueArmy.armyID));
+    blueArmy.battalions.push_back(generate_batalion(5,130,1,50,0,0,blueArmy.armyID));
+    
+    redArmy.battalions.push_back(generate_batalion(4,150,1,50,1,10,redArmy.armyID));
+    redArmy.battalions.push_back(generate_batalion(5,130,1,50,0,0,redArmy.armyID));
+    redArmy.battalions.push_back(generate_batalion(5,130,1,50,0,0,redArmy.armyID));
+    redArmy.battalions.push_back(generate_batalion(5,130,1,50,0,0,redArmy.armyID));
 
     
-    //executeSim
-    
+    //executeSim(blueArmy, redArmy, map, 720/*1 month in hours*/);
+
 
 
     
